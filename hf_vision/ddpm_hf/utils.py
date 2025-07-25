e03f80571ae208d68d5203e574b21f1f8a8b1da4
Mircea <1849908+mcimpoi@users.noreply.github.com>
2025-07-25 12:58:45 +0200

    WIP + add gitignore

--- hf_vision/ddpm_hf/utils.py
+++ hf_vision/ddpm_hf/utils.py
@@ -1,6 +1,9 @@
 import torch
 import numpy as np
 from torchvision import transforms
+import torch.nn.functional as F
+
+from datasets import load_dataset
 
 IMAGE_SIZE: int = 128
 N_TIMESTEPS: int = 300
@@ -43,16 +46,70 @@ def extract(a, t, x_shape):
     out = a.gather(-1, t.cpu())
     return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
 
+
 def q_sample(x_start, t, noise=None):
     if noise is None:
         noise = torch.randn_like(x_start)
 
     betas = linear_beta_schedule(N_TIMESTEPS)
-    alphas = 1. - betas
+    alphas = 1.0 - betas
     alphas_cumprod = torch.cumprod(alphas, dim=0)
     sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
-    sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - alphas_cumprod)
+    sqrt_one_minus_alphas_cumprod = torch.sqrt(1.0 - alphas_cumprod)
 
     sqrt_alphas_cumprod_t = extract(sqrt_alphas_cumprod, t, x_start.shape)
-    sqrt_one_minus_alphas_cumprod_t = extract(sqrt_one_minus_alphas_cumprod, t, x_start.shape)  
+    sqrt_one_minus_alphas_cumprod_t = extract(
+        sqrt_one_minus_alphas_cumprod, t, x_start.shape
+    )
     return sqrt_alphas_cumprod_t * x_start + sqrt_one_minus_alphas_cumprod_t * noise
+
+
+def get_noisy_image(x_start, t):
+    x_noisy = q_sample(x_start, t)
+    noisy_image = reverse_tensor_transform(x_noisy.squeeze())
+    return noisy_image
+
+
+def p_losses(denoise_model, x_start, t, noise=None, loss_type="l1"):
+    if noise is None:
+        noise = torch.randn_like(x_start)
+
+    x_noisy = q_sample(x_start, t, noise=noise)
+    predicted_noise = denoise_model(x_noisy, t)
+
+    if loss_type == "l1":
+        loss = F.l1_loss(predicted_noise, noise)
+    elif loss_type == "l2":
+        loss = F.mse_loss(predicted_noise, noise)
+    elif loss_type == "huber":
+        loss = F.smooth_l1_loss(predicted_noise, noise)
+    else:
+        raise NotImplementedError(f"Unknown loss type: {loss_type}")
+
+    return loss
+
+
+def get_fashion_mnist_dataloader(batch_size: int = 128) -> torch.utils.data.DataLoader:
+    transform = transforms.Compose(
+        [
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x * 2 - 1),  # Normalize to
+        ]
+    )
+
+    def transforms_fn(examples: dict) -> dict:
+        examples["pixel_values"] = [transform(image.convert("L")) for image in examples["image"]]
+        del examples["image"]
+        return examples
+    
+    dataset = load_dataset("fashion_mnist")
+    transformed_dataset = dataset.with_transform(transforms_fn).remove_columns("label")
+    dataloader = torch.utils.data.DataLoader(
+        transformed_dataset["train"],
+        batch_size=batch_size,
+        shuffle=True,
+    )
+
+    return dataloader
+    
---
+++ hf_vision/ddpm_hf/visualization.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+
+# Adapted from:
+# https://huggingface.co/blog/annotated-diffusion
+# which adapted it from https://github.com/pytorch/vision/blob/main/gallery/transforms/helpers.py
+
+
+def plot_images(
+    images: list,
+    original_images: list | None = None,
+    row_title: str | None = None,
+    **imshow_kwargs: dict,
+) -> None:
+    if not isinstance(images[0], list):
+        images = [images]
+
+    n_rows = len(images)
+    n_cols = len(images[0]) + (1 if original_images is not None else 0)
+    print(f"{n_rows=}, {n_cols=}")
+
+    fig, axs = plt.subplots(
+        figsize=(n_cols * 5, n_rows * 5), nrows=n_rows, ncols=n_cols, squeeze=False
+    )
+
+    for row_idx, row in enumerate(images):
+        with_original_image = (
+            original_images is not None
+            and len(original_images) > row_idx
+            and original_images[row_idx] is not None
+        )
+        row = [original_images[row_idx]] + row if with_original_image else row
+        for col_idx, img in enumerate(row):
+            ax = axs[row_idx, col_idx]
+            ax.imshow(img, **imshow_kwargs)
+            ax.set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+
+        if row_title is not None and col_idx == 0:
+            ax.set_title(row_title)
+        if with_original_image:
+            axs[row_idx, 0].set_title("Original Image")
+    plt.tight_layout()
